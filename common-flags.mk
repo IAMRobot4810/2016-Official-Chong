f7fe0ce07769cf639af90e5a211146b1d9dea50f
Joe Maples <frap129@gmail.com>
2015-08-30 20:29:04 -0400

    Add a core flag deffinitions file

---
+++ common-flags.mk
@@ -0,0 +1,39 @@
+# Copyright IAMRobot 2015-2016 (c)
+
+# Define some common flags to be included in Makefile.
+# These flags may include syntax fixes, warning enhancements, and optimizations.
+WORKLOAD := -fipa-sra \
+            -fgcse-sm \
+            -fgcse-las \
+            -fgcse-after-reload \
+            -fmodulo-sched \
+            -fira-loop-pressure \
+            -fira-hoist-pressure
+
+MULTITHREAD := -fopenmp \
+               -ftree-parallelize-loops=2 \
+               -ftree-parallelize-all
+
+LTO := -flto=4
+
+GRAPHITE := -fgraphite \
+            -fgraphite-identity
+
+MEMLEAK := -fsanitize=leak
+
+HARDWARE := -march=armv7-a \
+            -mcpu=cortex-a9 \
+            -mtune=cortex-a9 \
+            -marm
+
+DEBUG := -Og \
+         -g3 \
+         -Wall \
+         -Werror \
+         -DDEBUG \
+         -std=gnu11
+         
+RELEASE := -O3 \
+           -g0 \
+           -DNDEBUG \
+           -std=gnu11
