29271751ccedc1e54c0022fd4ddc516e07bad3f8
Joe Maples <frap129@gmail.com>
2015-12-10 17:24:05 -0500

    flags: Completely reorganize

--- common-flags.mk
+++ common-flags.mk
@@ -2,30 +2,26 @@
 
 # Define some common flags to be included in Makefile.
 # These flags may include syntax fixes, warning enhancements, and optimizations.
-WORKLOAD := -fipa-sra \
+GRAPHITE := -fgraphite \
+            -fgraphite-identity 
+HARDWARE := -march=armv7-a \
+            -mcpu=cortex-a9 \
+            -mtune=cortex-a9 \
+            -marm
+LOOP     := -fipa-sra \
             -fgcse-sm \
             -fgcse-las \
             -fgcse-after-reload \
             -fmodulo-sched \
             -fira-loop-pressure \
             -fira-hoist-pressure
+LSAN     := -fsanitize=leak
+LTO      := -flto=2
+OPENMP   := -fopenmp \
+            -ftree-parallelize-loops=2 \
+            -ftree-parallelize-all
 
-MULTITHREAD := -fopenmp \
-               -ftree-parallelize-loops=2 \
-               -ftree-parallelize-all
-
-LTO := -flto=2
-
-GRAPHITE := -fgraphite \
-            -fgraphite-identity 
-
-MEMLEAK := -fsanitize=leak
-
-HARDWARE := -march=armv7-a \
-            -mcpu=cortex-a9 \
-            -mtune=cortex-a9 \
-            -marm
-
+# Create two sets of flags, debug and release, for easy incorporation into build.
 DEBUG := -Og \
          -g3 \
          -Wall \
@@ -38,10 +34,9 @@ RELEASE := -O3 \
            -g0 \
            -DNDEBUG \
            -std=gnu11 \
-           $(WORKLOAD)\
-           $(MULTITHREAD) \
-           $(LTO) \
            $(GRAPHITE) \
-           $(MEMLEAK) \
-           $(HARDWARE)
-           
+           $(HARDWARE) \
+           $(LOOP)\
+           $(LSAN) \
+           $(LTO) \
+           $(OPENMP)
